cb23cb22e00fb8692f6fac94084992443efa001d
tophf <tophf@gmx.com>
2022-11-11 20:45:06 +0300

    fix: add TypedArray

--- index.d.ts
+++ index.d.ts
@@ -298,6 +298,19 @@ declare interface VMScriptResponseObject<T> {
   context?: unknown;
 }
 
+type TypedArray =
+    | Uint8Array
+    | Uint8ClampedArray
+    | Uint16Array
+    | Uint32Array
+    | Int8Array
+    | Int16Array
+    | Int32Array
+    | BigUint64Array
+    | BigInt64Array
+    | Float32Array
+    | Float64Array;
+
 interface GMRequestBase<T> {
   /** URL relative to current page is also allowed. */
   url: string;
